2071c4d14cae1ad73ef1a17f168125ce9055dc0a
rutgerterheide <rutger.ter.heide@randstadgroep.nl>
2025-02-05 17:26:26 +0100

    added LicenseValidationControllerTest

---
+++ src/test/java/com/bovemij/kentekenplaat/check/demo/controllers/LicenseValidationControllerTest.java
@@ -0,0 +1,31 @@
+package com.bovemij.kentekenplaat.check.demo.controllers;
+
+import com.bovemij.kentekenplaat.check.demo.services.LicenseValidationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class LicenseValidationControllerTest {
+
+	@Mock
+	private LicenseValidationService licenseValidationService;
+
+	@InjectMocks
+	private LicenseValidationController licenseValidationController;
+
+	@Test
+	public void testValidateLicensePlate() {
+		final String licensePlate = "123456789";
+
+		licenseValidationController.validateLicensePlate("123456789");
+
+		verify(licenseValidationService, times(1)).validateLicense(licensePlate);
+	}
+
+}
